f76abb6e73f12a4d9b706c576ffb3a057c1036c3
Eftal Gezer <eftalgezer@gmail.com>
2023-08-08 19:13:50 +0300

    Add files via upload

--- tests/testers.py
+++ tests/testers.py
@@ -15,7 +15,34 @@ import os
 import unittest
 from unittest.mock import patch
 import tkinter as tk
+from tkinter import scrolledtext
 from TkConsole import Console
+
+
+class FakeTk(tk.Tk):
+    def __init__(self):
+        super(FakeTk, self).__init__()
+
+    def mainloop(self, **kwargs):
+        pass
+
+    class Entry(tk.Entry):
+        def __init__(self):
+            super(FakeTk.Entry, self).__init__()
+
+        def mainloop(self, n: int = ...) -> None:
+            pass
+
+
+class FakeScrolledText(scrolledtext.ScrolledText):
+    def __init__(self, master=None, **kw):
+        super(FakeScrolledText, self).__init__()
+
+    def mainloop(self, n: int = ...) -> None:
+        pass
+
+    def winfo_height(self) -> int:
+        return 500
 
 
 class ConsoleTest(unittest.TestCase):
@@ -26,11 +53,11 @@ class ConsoleTest(unittest.TestCase):
 
     Methods:
         setUp(self): Set up the test environment.
+        tearDown(self): Clean up the test environment after each test.
         test_print_output(self): Test the print method's output to the text area.
         test_input(self): Test the input method by mocking the built-in input function.
         test_copy_text(self): Test the copy_text method's functionality.
         test_paste_text(self): Test the paste_text method's functionality.
-        tearDown(self): Clean up the test environment after each test.
     """
 
     def setUp(self):
@@ -46,71 +73,16 @@ class ConsoleTest(unittest.TestCase):
         if os.name != "nt" and os.getenv("GITHUB_ACTIONS"):
             os.system('Xvfb :1 -screen 0 1600x1200x16  &')
             os.environ["DISPLAY"] = ":1.0"
-        root = tk.Tk()
-        self.console = Console(root)
+        self.root = FakeTk()
+        self.console = Console(self.root)
+        self.console.text_area = FakeScrolledText(
+            self.console.parent, wrap=tk.WORD, font=self.console.font, background=self.console.background,
+            foreground=self.console.foreground, padx=0,
+            pady=0, borderwidth=0, border=0.0, insertborderwidth=0, selectborderwidth=0
+        )
         self.text_area = self.console.text_area
         self.entry = self.console.entry
         self.user_input_var = self.console.user_input_var
-
-    def test_print_output(self):
-        """
-        Test the print method's output to the text area.
-
-        This method tests whether the print method correctly inserts text into the text area.
-
-        Returns:
-            None
-        """
-        expected_output = "Hello, world! This terminal-like library is great.\n"
-        self.console.print("Hello, world!", end=" ")
-        self.console.print("This terminal-like library is great.")
-        self.assertEqual(self.text_area.get("1.0", tk.END), expected_output)
-
-    def test_input(self):
-        """
-        Test the input method by mocking the built-in input function.
-
-        This method uses the unittest.mock.patch decorator to mock the built-in input function,
-        allowing for controlled input during testing.
-
-        Returns:
-            None
-        """
-        with patch("builtins.input", return_value="42"):
-            user_input = self.console.input("Enter a number: ")
-            self.assertEqual(user_input, "42")
-
-    def test_copy_text(self):
-        """
-        Test the copy_text method's functionality.
-
-        This method tests whether the copy_text method correctly copies selected text from the text area to the
-        clipboard.
-
-        Returns:
-            None
-        """
-        self.text_area.insert("1.0", "Copy this text.")
-        self.text_area.tag_add(tk.SEL, "1.0", "1.10")
-        self.console.copy_text()
-        clipboard_text = self.console.parent.clipboard_get()
-        self.assertEqual(clipboard_text, "Copy this ")
-
-    def test_paste_text(self):
-        """
-        Test the paste_text method's functionality.
-
-        This method tests whether the paste_text method correctly pastes text from the clipboard to the entry widget.
-
-        Returns:
-            None
-        """
-        clipboard_text = "Pasted text."
-        self.console.parent.clipboard_clear()
-        self.console.parent.clipboard_append(clipboard_text)
-        self.console.input()
-        self.console.paste_text()
-        self.assertEqual(self.entry.get(), clipboard_text)
 
     def tearDown(self):
         """
@@ -122,8 +94,84 @@ class ConsoleTest(unittest.TestCase):
         Returns:
             None
         """
-        if self.console:
-            self.console.parent.destroy()
+        if self.root:
+            self.root.destroy()
+
+    def test_print_output(self):
+        """
+        Test the print method's output to the text area.
+
+        This method tests whether the print method correctly inserts text into the text area.
+
+        Returns:
+            None
+        """
+        self.setUp()
+        expected_output = "Hello, world! This terminal-like library is great.\n"
+        self.console.print("Hello, world!", end=" ")
+        self.console.print("This terminal-like library is great.")
+        self.assertEqual(self.text_area.get("1.0", tk.END), expected_output)
+        self.root.update_idletasks()
+        self.tearDown()
+        self.root = None
+
+    def test_input(self):
+        """
+        Test the input method by mocking the built-in input function.
+
+        This method uses the unittest.mock.patch decorator to mock the built-in input function,
+        allowing for controlled input during testing.
+
+        Returns:
+            None
+        """
+        self.setUp()
+        with patch("builtins.input", return_value="42"):
+            user_input = self.console.input("Enter a number: ")
+            self.assertEqual(user_input, "42")
+        self.root.update_idletasks()
+        self.tearDown()
+        self.root = None
+
+    def test_copy_text(self):
+        """
+        Test the copy_text method's functionality.
+
+        This method tests whether the copy_text method correctly copies selected text from the text area to the
+        clipboard.
+
+        Returns:
+            None
+        """
+        self.setUp()
+        self.text_area.insert("1.0", "Copy this text.")
+        self.text_area.tag_add(tk.SEL, "1.0", "1.10")
+        self.console.copy_text()
+        clipboard_text = self.console.parent.clipboard_get()
+        self.assertEqual(clipboard_text, "Copy this ")
+        self.root.update_idletasks()
+        self.tearDown()
+        self.root = None
+
+    def test_paste_text(self):
+        """
+        Test the paste_text method's functionality.
+
+        This method tests whether the paste_text method correctly pastes text from the clipboard to the entry widget.
+
+        Returns:
+            None
+        """
+        self.setUp()
+        clipboard_text = "Pasted text."
+        self.console.parent.clipboard_clear()
+        self.console.parent.clipboard_append(clipboard_text)
+        self.console.input()
+        self.console.paste_text()
+        self.assertEqual(self.entry.get(), clipboard_text)
+        self.root.update_idletasks()
+        self.tearDown()
+        self.root = None
 
 
 if __name__ == '__main__':
